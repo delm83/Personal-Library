c7125e1d8e8dccb2b67e75dc539b70ab22b8b694
delm83 <delm83@googlemail.com>
2024-02-09 12:35:37 +0000

    did api/books post function

--- routes/api.js
+++ routes/api.js
@@ -10,14 +10,44 @@
 
 module.exports = function (app) {
 
+  const mongoose = require('mongoose');
+  mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+  let Schema = mongoose.Schema;
+
+  let bookSchema = new Schema({
+      title: {
+      type: String,
+      required: true,
+      default: ''
+    },
+      comments: {
+      type: Array,
+      default: []
+    }
+ });
+
   app.route('/api/books')
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
     
-    .post(function (req, res){
-      let title = req.body.title;
+    .post(async (req, res)=>{
+      try{
+        let Book = mongoose.model("Book", bookSchema);
+        let title = req.body.title;
+
+        if(!title){
+          return res.type('txt').send('missing required field title');
+        }
+
+        let inputBook = new Book({
+         title: title
+ });
+     await inputBook.save();
+     res.json({title: inputBook.title, _id: inputBook._id});
+    }catch(err){return res.json({error: err})}
       //response will contain new book object including atleast _id and title
     })
     
