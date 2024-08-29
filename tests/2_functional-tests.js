3bc909b64852ea99381edd68bdaf573c5030c008
delm83 <delm83@googlemail.com>
2024-08-29 14:32:49 +0100

    commented out example test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -21,7 +21,7 @@ suite('Functional Tests', function() {
   * ----[EXAMPLE TEST]----
   * Each test should completely test the response of the API end-point including response status code!
   */
-  test('#example Test GET /api/books', function(done){
+  /*test('#example Test GET /api/books', function(done){
      chai.request(server)
       .get('/api/books')
       .end(function(err, res){
@@ -32,7 +32,7 @@ suite('Functional Tests', function() {
         assert.property(res.body[0], '_id', 'Books in array should contain _id');
         done();
       });
-  });
+  });*/
   /*
   * ----[END of EXAMPLE TEST]----
   */
